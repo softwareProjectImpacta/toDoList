ef4373023861753ad0973409186302cc870b11bd
Sheilahilario <sheila.oh08@gmail.com>
2023-08-28 16:10:02 -0300

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require("path");
+
+const app = express()
+const port = 3000;
+
+app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, "shared")))
+
+//ROTA BACK-END
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => console.log(`Servidor rodando em:${port}`))
