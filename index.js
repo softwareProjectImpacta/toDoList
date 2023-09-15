0bbe2eea03eadc03cf613f88aeaffe7b1966f83b
turibarros <turi.barros@gmail.com>
2023-09-15 19:51:44 -0300

    To Do List conectando/inserindo dados
    Realizado a conxão ao BD, inserindo, listando e persistindo dados informados via servidor local.

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const express = require('express')
-const path = require("path");
-const routes = require("./routes/routes")
-
-const app = express()
+const express = require('express');
+const path = require('path');
+const routes = require('./routes/routes');
+const mssql = require('./database/db');
+const bodyParser = require('body-parser');
+const app = express();
+const TaskController = require('./controller/TaskController');
 const port = 3000;
+TaskController.connectToDatabase();
+
+// Configuração do body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //Back-end
+app.get('/home', (req, res) => {
+    res.render('index');
+});
 
-app.set("view engine", "ejs")
-app.use(express.static(path.join(__dirname, "shared")))
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'shared')));
 app.use(routes)
+app.listen(port, () => console.log(`Servidor rodando na porta:${port}`));
 
-app.listen(port, () => console.log(`Servidor rodando em:${port}`))
+
